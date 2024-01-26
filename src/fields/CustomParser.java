0bd7604097c9d0fc7619926b8a5cdc7596dc1293
FADILI EL MOSTAFA <fadilimustafa530@gmail.com>
2024-01-26 16:23:08 +0100

    @fadili: create object from property file

---
+++ src/fields/CustomParser.java
@@ -0,0 +1,60 @@
+package fields;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class CustomParser {
+
+	public static <T> T createObjectFromFile(Class<T> clazz, Path fielPath) throws IOException, NoSuchMethodException,
+			InstantiationException, InvocationTargetException, IllegalAccessException {
+
+		Constructor<T> constructor = clazz.getDeclaredConstructor();
+		T object = constructor.newInstance();
+
+		try (Scanner scanner = new Scanner(fielPath)) {
+
+			while (scanner.hasNextLine()) {
+				String[] fieldNameValuePair = scanner.nextLine().split("=");
+				if (fieldNameValuePair.length != 2)
+					continue;
+
+				String fieldName = fieldNameValuePair[0];
+				String fieldValue = fieldNameValuePair[1];
+
+				Field field;
+				try {
+					field = clazz.getDeclaredField(fieldName);
+				} catch (NoSuchFieldException | SecurityException e) {
+					System.out.println(String.format("Property name : %s is unsupported", fieldName));
+					continue;
+				}
+				field.setAccessible(true);
+				Object parsedValue = parseValue(field.getType(), fieldValue);
+				field.set(object, parsedValue);
+			}
+		}
+
+		return object;
+	}
+
+	private static Object parseValue(Class<?> type, String fieldValue) {
+		if (type.equals(int.class)) {
+			return Integer.parseInt(fieldValue);
+		} else if (type.equals(short.class)) {
+			return Short.parseShort(fieldValue);
+		} else if (type.equals(long.class)) {
+			return Long.parseLong(fieldValue);
+		} else if (type.equals(double.class)) {
+			return Double.parseDouble(fieldValue);
+		} else if (type.equals(float.class)) {
+			return Float.parseFloat(fieldValue);
+		} else if (type.equals(String.class)) {
+			return fieldValue;
+		}
+		throw new RuntimeException(String.format("Type : %s unsupported", type.getTypeName()));
+	}
+}
--- src/fields/Main.java
+++ src/fields/Main.java
@@ -1,17 +1,28 @@
 package fields;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Path;
+
 public class Main {
 
-	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
-		Company company = new Company("Udemy", "San Francisco", new Address("Harrison Street", (short) 600));
-		Address address = new Address("Main Street", (short) 1);
-		Person person = new Person("John", true, 29, 100.555f, address, company);
-		System.out.println(CustomSerializer.serialize(person, 1));
+	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InstantiationException, InvocationTargetException, IOException {
+//		Company company = new Company("Udemy", "San Francisco", new Address("Harrison Street", (short) 600));
+//		Address address = new Address("Main Street", (short) 1);
+//		Person person = new Person("John", true, 29, 100.555f, address, company);
+//		System.out.println(CustomSerializer.serialize(person, 1));
 //		long sizeOfObject = ObjectSizeCalculator.sizeOfObject(person);
 //		System.out.println(sizeOfObject);
-		int[][] arr = {{1, 2, 3}, {1, 2}};
-		String array = ArrayInspector.inspect(arr);
-		System.out.println(array);
+//		int[][] arr = {{1, 2, 3}, {1, 2}};
+//		String array = ArrayInspector.inspect(arr);
+//		System.out.println(array);
+		
+		final Path GAME_CONFIG_PATH = Path.of("resources/game-properties.cfg");
+	    final Path UI_CONFIG_PATH = Path.of("resources/user-interface.cfg");
+		//GameConfig config = CustomParser.createObjectFromFile(GameConfig.class, GAME_CONFIG_PATH);
+		UserInterfaceConfig config = CustomParser.createObjectFromFile(UserInterfaceConfig.class, UI_CONFIG_PATH);
+
+        System.out.println(config);
 	}
 
 }
